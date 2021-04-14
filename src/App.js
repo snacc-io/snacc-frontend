81df0911949a3ea6ca9b17dc51e2679ad58b2928
Ryan Persson <ryanpersson.dev@gmail.com>
2021-04-13 19:01:32 -0700

    Added login functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
-import Header from "./Header"
+import Header from "./Header";
+import Login from "./Login";
 
 function App() {
   return (
