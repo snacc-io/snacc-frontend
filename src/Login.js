6bcd6cfe9c49681b1a21a07fdd2b66716f720862
Ryan Persson <ryanpersson.dev@gmail.com>
2021-04-28 19:57:47 -0700

    Got rid of amazon login

--- src/Login.js
+++ src/Login.js
@@ -37,13 +37,7 @@ function Login() {
 
     return (
         <div className='login'>
-            <Link to='/'>
-                <img
-                    className="login__logo"
-                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' 
-                    alt=""
-                />
-            </Link>
+
 
             <div className='login__container'>
                 <h1>Sign-in</h1>
@@ -59,11 +53,10 @@ function Login() {
                 </form>
 
                 <p>
-                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
-                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                    By creating an account you agree to the terms of use for Snacc.io simple recipe website
                 </p>
 
-                <button onClick={register} className='login__registerButton'>Create your Amazon Account</button>
+                <button onClick={register} className='login__registerButton'>Create Your Account/login</button>
             </div>
         </div>
     )
