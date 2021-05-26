002445467ec178724375c1d40f35e24442718cbc
Lyon-K <keelyon2000@gmail.com>
2021-05-26 22:42:20 +0800

    Fixed INSERT Query & package-lock installation updates

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.get("/api/:table", (req, res) => {
 });
 
 app.post("/api/:table/Insert", (req, res) => {
-  const SQLQuery = `INSERT INTO ${req.params.table}(recipeName, recipeDescription, recipeImageURL, recipeInstructions, cookingTime, userID) VALUES(${req.body.nameInput}, ${req.body.descriptionInput}, ${req.body.imageURL}, ${req.body.instructionsInput}, ${req.body.cookingTimeInput}, ${req.body.userIDInput});`;
+  const SQLQuery = `INSERT INTO ${req.params.table}(recipeID, recipeName, recipeDescription, recipeImageURL, recipeInstructions, cookingTime, views, rating, userID) VALUES(${req.body.recipeID}, \"${req.body.recipeName}\", \"${req.body.recipeDescription}\", \"${req.body.recipeImageURL}\", \"${req.body.recipeInstructions}\", ${req.body.cookingTime}, ${req.body.views}, ${req.body.rating}, ${req.body.userID});`;
 
   db.query(SQLQuery, (err, result) => {
     if (err) console.log(err);
